f0b4720e78282e3addb423b50f063cf818d5826e
coderisinginfra <risinginfranoida@gmail.com>
2024-07-10 12:04:44 +0530

    Update AddProjectRouter.js

--- Backend/Routers/AddProjectRouter.js
+++ Backend/Routers/AddProjectRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const AddProjectController = require('../controllers/AddProjectController');
+const AddProjectController = require('../Controllers/AddProjectController');
 const upload = require('../Middleware/Multer');
 
 router.post('/addproject', upload.fields([
