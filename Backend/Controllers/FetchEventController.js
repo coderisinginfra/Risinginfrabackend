2dcbdf6976884d02176f2a869aeb556420be4596
sudhanshu <risinginfranoida@gmail.com>
2024-08-13 15:09:25 +0530

    add

--- Backend/Controllers/FetchEventController.js
+++ Backend/Controllers/FetchEventController.js
@@ -2,7 +2,7 @@ const express = require('express')
 const EventAddModel = require('../Models/EventAddModel')
 const FetchEventController = async(req,res)=>{
     try {
-        const result = EventAddModel.find()
+        const result = await EventAddModel.find()
         res.status(200).json(result)
     } catch (error) {
         res.status(500).json({message:`Invalid Credentails ${error}`})
