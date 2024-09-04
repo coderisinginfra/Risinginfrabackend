20c226ea7189146c98b6ab380c0866ae15d3fe6a
sudhanshu <risinginfranoida@gmail.com>
2024-09-04 10:56:48 +0530

    add

--- Backend/Controllers/BhutaniFetchController.js
+++ Backend/Controllers/BhutaniFetchController.js
@@ -2,8 +2,8 @@ const express = require('express')
 const AddProject = require('../Models/AddProject')
 const BhutaniFetchController = async(req,res)=>{
     try {
-        const result = await AddProject.find({developer: 'ACE Group'})
-        res.json(201).json(result)
+        const result = await AddProject.find({developer: 'Bhutani Infra'})
+        res.status(201).json(result)
     } catch (error) {
         res.status(500).json("Invalid Credentials")
     }
